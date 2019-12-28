7e309599fc5665f6c01d483685985d56bd9a4c27
Yagnavalk Chaapala <31382160+yvdevy@users.noreply.github.com>
2019-12-28 09:10:26 +0530

    Update HiGit.java

--- EclipseGitTest/src/HiGit/HiGit.java
+++ EclipseGitTest/src/HiGit/HiGit.java
@@ -5,6 +5,7 @@ public class HiGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hi Git this project is from eclipse");
+		System.out.println("Hi eclipse, I'm writing this line of code in Git");
 
 	}
 
